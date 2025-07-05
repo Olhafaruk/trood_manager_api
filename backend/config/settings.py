b4328144bc6bd8ab985da1b1ab39f43ff2d56626
Olha Faruk <farukolga2017@gmail.com>
2025-07-05 17:28:09 +0200

    feat: add API with serializers, viewsets, and routing for Project and Vacancy

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -3,23 +3,18 @@
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -29,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'projects',
     'vacancy',
 ]
--- backend/vacancy/views.py
+++ backend/vacancy/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#backend/vacancy/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Vacancy
+from .serializers import VacancySerializer
+
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
+
+
+
+
